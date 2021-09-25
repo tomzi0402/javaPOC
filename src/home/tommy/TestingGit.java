a48f5cf3e48b714a5142f7f09a369bbdf9818dc5
tomzi0402 <70941164+tomzi0402@users.noreply.github.com>
2021-09-26 01:08:35 +0800

    First commit to main

---
+++ src/home/tommy/TestingGit.java
@@ -0,0 +1,14 @@
+package home.tommy;
+
+public class TestingGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			System.out.print("My First Project in GitHub!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
