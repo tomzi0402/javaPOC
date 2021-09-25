3a37af388a627af9da67bec7a052709d4ed9db04
tomzi0402 <70941164+tomzi0402@users.noreply.github.com>
2021-09-26 01:18:09 +0800

    override first version

--- src/home/tommy/TestingGit.java
+++ src/home/tommy/TestingGit.java
@@ -5,10 +5,15 @@ public class TestingGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
-			System.out.print("My First Project in GitHub!");
+//			System.out.print("My First Project in GitHub!");
+			System.out.print(getHelloGitHub());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static String getHelloGitHub() {
+		return "Hello GitHub!";
 	}
 
 }
